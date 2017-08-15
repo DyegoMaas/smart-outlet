8383d75c93689c71b44997321d9dd5f550557151
Dyego Alekssander Maas <dyego.maas@gmail.com>
2017-08-14 22:49:50 -0300

    Testing other implementations.

--- LeituraTensao/LeituraTensao.ino
+++ LeituraTensao/LeituraTensao.ino
@@ -20,5 +20,170 @@ void loop()
 {
 	auto leitura = currentSensor.readAC(sensorIn);
 	Serial.print("potencia: ");
-	Serial.println(leitura.power);
+	Serial.print(leitura.power);
+
+	Serial.print("	corrente: ");
+	Serial.print(leitura.amps);
+
+	Serial.print("	tensao: ");
+	Serial.print(leitura.voltage);
+
+	Serial.println();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+////ACS712 Arduino Code
+//float vcc = 0;
+//
+//void setup() {
+//	Serial.begin(9600);
+//}
+//
+//
+//long readVcc() {
+//	// Read 1.1V reference against AVcc
+//	// set the reference to Vcc and the measurement to the internal 1.1V reference
+//#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+//	ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+//#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+//	ADMUX = _BV(MUX5) | _BV(MUX0);
+//#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+//	ADMUX = _BV(MUX3) | _BV(MUX2);
+//#else
+//	ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+//#endif
+//
+//	delay(2); // Wait for Vref to settle
+//	ADCSRA |= _BV(ADSC); // Start conversion
+//	while (bit_is_set(ADCSRA, ADSC)); // measuring
+//
+//	uint8_t low = ADCL; // must read ADCL first – it then locks ADCH
+//	uint8_t high = ADCH; // unlocks both
+//
+//	long result = (high << 8) | low;
+//
+//	result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+//	return result; // Vcc in millivolts
+//}
+//
+//
+//void loop() {
+//	vcc = readVcc() / 1000.0;
+//	Serial.print("Vcc: ");
+//	Serial.print(vcc);
+//
+//	long average = 0;
+//	for (int i = 0; i < 100; i++) {
+//		average = average + analogRead(A0);
+//		delay(1);
+//	}
+//	average = average / 100;
+//
+//	float sensorValue = average * (5.0 / 1023.0);
+//	Serial.print(" sense : ");
+//	Serial.print(sensorValue, 3);
+//
+//	float acoffset = vcc / 2.0;
+//	Serial.print(" offst : ");
+//	Serial.print(acoffset, 3);
+//
+//	float sensitivity = 0.185 * (vcc / 5.0);
+//	Serial.print(" sensi : ");
+//	Serial.print(sensitivity, 4);
+//
+//	float amps = (sensorValue - acoffset) / sensitivity;
+//	Serial.print(" Amperes : ");
+//	Serial.print(amps);
+//	Serial.println("…");
+//	delay(1000);
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+//#define CURRENT_SENSOR A0  // Define Analog input pin that sensor is attached
+//
+//float amplitude_current;      // Float amplitude current
+//float effective_value;       // Float effective current
+//
+//void pins_init()
+//{
+//	pinMode(CURRENT_SENSOR, INPUT);
+//}
+//
+//void setup()
+//{
+//	Serial.begin(9600);
+//	pins_init();
+//}
+//
+//
+///*Function: Sample for 1000ms and get the maximum value from the S pin*/
+//
+//int getMaxValue()
+//{
+//	int sensorValue;    //value read from the sensor
+//	int sensorMax = 0;
+//	uint32_t start_time = millis();
+//	while ((millis() - start_time) < 1000) //sample for 1000ms
+//	{
+//		sensorValue = analogRead(CURRENT_SENSOR);
+//		if (sensorValue > sensorMax)
+//		{
+//			/*record the maximum sensor value*/
+//
+//			sensorMax = sensorValue;
+//		}
+//	}
+//	return sensorMax;
+//}
+//
+//void loop()
+//{
+//	int sensor_max;
+//	sensor_max = getMaxValue();
+//	Serial.print("sensor_max = ");
+//	Serial.println(sensor_max);
+//
+//	//the VCC on the Arduino interface of the sensor is 5v
+//
+//	amplitude_current = (float)(sensor_max - 512) / 1024 * 5 / 185 * 1000000; // for 5A mode,you need to modify this with 20 A and 30A mode;
+//	effective_value = amplitude_current / 1.414;
+//
+//	//for minimum current=1/1024*5/185*1000000/1.414=18.7(mA)
+//	//Only sinusoidal alternating current
+//
+//	Serial.println("The amplitude of the current is(in mA)");
+//	Serial.println(amplitude_current, 1);
+//
+//	//Only one number after the decimal point
+//
+//	Serial.println("The effective value of the current is(in mA)");
+//	Serial.println(effective_value, 1);
+//}
+
+
+
+
+
+
+
+
--- LeituraTensao/SensoresCorrente.cpp
+++ LeituraTensao/SensoresCorrente.cpp
@@ -4,6 +4,7 @@ using namespace SensoresCorrente;
 
 ACS712Class::ACS712Class(ModuleType moduleType)
 {
+	//TODO verificar se é isso mesmo
 	switch (moduleType)
 	{
 		case _30A: mVperAmp = 66; break;
@@ -21,7 +22,25 @@ DCReading ACS712Class::readDC(int analogInPin) const
 	auto power = Utils::computePower(amps, voltage);
 	return DCReading(amps, voltage, power);
 }
-
+//
+//int getMaxValue(int analogInPin)
+//{
+//	int sensorValue;    //value read from the sensor
+//	int sensorMax = 0;
+//	uint32_t start_time = millis();
+//	while ((millis() - start_time) < 1000) //sample for 1000ms
+//	{
+//		sensorValue = analogRead(analogInPin);
+//		if (sensorValue > sensorMax)
+//		{
+//			/*record the maximum sensor value*/
+//
+//			sensorMax = sensorValue;
+//		}
+//	}
+//	return sensorMax;
+//}
+//
 float getVPP(int analogInPin)
 {
 	auto maxValue = 0;          // store max value here
@@ -52,6 +71,50 @@ float getVPP(int analogInPin)
 
 ACReading ACS712Class::readAC(int analogInPin) const
 {
+	//auto sensor_max = getMaxValue(analogInPin);
+	//Serial.print("sensor_max = ");
+	//Serial.println(sensor_max);
+
+	////the VCC on the Arduino interface of the sensor is 5v
+
+	//auto amplitude_current = (float)(sensor_max - 512) / 1024 * 5 / 185 * 1000000; // for 5A mode,you need to modify this with 20 A and 30A mode;
+	//auto effective_value = amplitude_current / 1.414;
+
+	////for minimum current=1/1024*5/185*1000000/1.414=18.7(mA)
+	////Only sinusoidal alternating current
+
+	//Serial.println("The amplitude of the current is(in mA)");
+	//Serial.println(amplitude_current, 1);
+
+	////Only one number after the decimal point
+
+	//Serial.println("The effective value of the current is(in mA)");
+	//Serial.println(effective_value, 1);
+
+	//return ACReading(effective_value * 1000, VRMS, power);
+
+
+
+
+
+	/*auto amplitude = getVPP(analogInPin);
+	Serial.print("amplitude: ");
+	Serial.print(amplitude);
+	Serial.print("	");
+
+	auto VRMS = (amplitude / 2.0) * 0.707;
+	auto ampsRms = (VRMS * 1000) / mVperAmp;
+
+	auto power = ampsRms * (VRMS * 1000);
+	return ACReading(ampsRms, VRMS, power);*/
+
+
+
+
+
+
+
+
 	auto voltageAmplitude = getVPP(analogInPin);
 	auto VRMS = Utils::computeVrms(voltageAmplitude);
 	auto vrmsVolts = VRMS * 1000;

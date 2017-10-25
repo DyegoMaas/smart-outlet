e1e38b8e31489267695e55968ee6047ca95406a1
Dyego Alekssander Maas <dyego.maas@gmail.com>
2017-10-25 12:13:27 -0200

    Adicionando TopShelf e Nancy ao serviço.

---
+++ Aplicacao/SmartOutlet.Outlet/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartOutlet.Outlet
+{
+    public class Class1
+    {
+        
+    }
+}
---
+++ Aplicacao/SmartOutlet.Service/Program.cs
@@ -0,0 +1,47 @@
+﻿using Topshelf;
+using Topshelf.Nancy;
+
+namespace SmartOutlet.Service
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = HostFactory.New(x =>
+            {
+//                x.UseNLog();
+	    
+                x.Service<Service>(s =>
+                {
+                    s.ConstructUsing(settings => new Service());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                    s.WithNancyEndpoint(x, c =>
+                    {
+                        c.AddHost(port: 8001, path:"smart-things/");
+                        c.ConfigureNancy(hc =>
+                        {
+                            hc.UrlReservations.CreateAutomatically = true;
+                        });
+                    });
+                });
+                x.StartAutomatically();
+                x.SetServiceName("topshelf.nancy.sampleservice");
+                x.RunAsNetworkService();
+            });
+	
+            host.Run();
+        }
+    }
+
+    internal class Service
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}

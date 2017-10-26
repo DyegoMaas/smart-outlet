67d27cf7eaaa48be975bf8efa2b86434ae01df0d
Dyego Alekssander Maas <dyego.maas@gmail.com>
2017-10-25 22:32:42 -0200

    Rodando o serviço finalmente.

--- Aplicacao/SmartOutlet.Service/Program.cs
+++ Aplicacao/SmartOutlet.Service/Program.cs
@@ -1,5 +1,8 @@
-﻿using Topshelf;
-using Topshelf.Nancy;
+﻿using System;
+using Nancy.Hosting.Self;
+using Quartz;
+using Quartz.Impl;
+using Topshelf;
 
 namespace SmartOutlet.Service
 {
@@ -11,22 +14,34 @@ namespace SmartOutlet.Service
             {
 //                x.UseNLog();
 	    
-                x.Service<Service>(s =>
+                x.Service<NancyService>(s =>
                 {
-                    s.ConstructUsing(settings => new Service());
+                    s.ConstructUsing(settings => new NancyService());
                     s.WhenStarted(service => service.Start());
                     s.WhenStopped(service => service.Stop());
-                    s.WithNancyEndpoint(x, c =>
-                    {
-                        c.AddHost(port: 8001, path:"smart-things/");
-                        c.ConfigureNancy(hc =>
-                        {
-                            hc.UrlReservations.CreateAutomatically = true;
-                        });
-                    });
+                    
+//                    s.WithNancyEndpoint(x, c =>
+//                    {
+//                        c.AddHost(port: 8001/*, path:"smart-things/"*/);
+//                        c.ConfigureNancy(hc =>
+//                        {
+//                            hc.UrlReservations.CreateAutomatically = true;
+//                        });
+//                    });
+                    
+//                    s.ScheduleQuartzJob(q =>
+//                        q.WithJob(() =>
+//                                JobBuilder.Create<MyJob>().Build())
+//                            .AddTrigger(() => TriggerBuilder.Create()
+//                                .WithSimpleSchedule(b => b
+//                                    .WithIntervalInSeconds(10)
+//                                    .RepeatForever())
+//                                .Build()));
                 });
                 x.StartAutomatically();
                 x.SetServiceName("topshelf.nancy.sampleservice");
+                x.SetDisplayName("SmartThings");
+                x.SetDescription("SmartThings - plugue inteligente");
                 x.RunAsNetworkService();
             });
 	
@@ -34,14 +49,57 @@ namespace SmartOutlet.Service
         }
     }
 
-    internal class Service
+    internal class MyJob : IJob
     {
+        public void Execute(IJobExecutionContext context)
+        {
+            int a = 0;
+        }
+    }
+
+    internal class NancyService
+    {
+        private const string ServiceUri = "http://localhost:8001/smart-things/";
+        private readonly NancyHost _nancyHost;
+        private readonly IScheduler _scheduler;
+
+        public NancyService()
+        {
+            _nancyHost = new NancyHost(new Uri(ServiceUri));
+            _scheduler = new StdSchedulerFactory().GetScheduler();
+        }
+
         public void Start()
         {
+            ConfigureNancy();
+            ConfigureJob();
+        }
+
+        private void ConfigureNancy()
+        {
+            
+            _nancyHost.Start();
+            Console.WriteLine($"Running on {ServiceUri}/");
+        }
+
+        private void ConfigureJob()
+        {
+            var job = JobBuilder.Create<MyJob>().Build();
+            var trigger = TriggerBuilder
+                .Create()
+//                .WithSimpleSchedule(x => x
+//                    .WithIntervalInSeconds(10)
+//                    .RepeatForever())
+                .StartNow()
+                .Build();
+            _scheduler.ScheduleJob(job, trigger);
+            _scheduler.Start();
         }
 
         public void Stop()
         {
+            _nancyHost.Dispose();
+            _scheduler.Shutdown();
         }
     }
 }

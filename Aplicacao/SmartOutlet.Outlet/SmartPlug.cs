14e0c5f78cca5d1d22291fdda45c7568211d99cc
Dyego Alekssander Maas <dyego.maas@gmail.com>
2017-10-26 10:15:32 -0200

    Adicionando Marten e começando módulos para controle do dispositivo.

---
+++ Aplicacao/SmartOutlet.Outlet/SmartPlug.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartOutlet.Outlet
+{
+    public interface ISmartPlug
+    {
+        ToggeResult TurnOff();
+        ToggeResult TurnOn();
+    }
+
+    public class SmartPlug : ISmartPlug
+    {
+        public ToggeResult TurnOff()
+        {
+            return new ToggeResult(PlugState.Off);
+        }
+
+        public ToggeResult TurnOn()
+        {
+            return new ToggeResult(PlugState.On);
+        }
+    }
+
+    public class ToggeResult
+    {
+        public PlugState State { get; }
+
+        public ToggeResult(PlugState state)
+        {
+            State = state;
+        }
+    }
+
+    public enum PlugState
+    {
+        Off = 0,
+        On = 1
+    }
+}
---
+++ Aplicacao/SmartOutlet.Service/Modules/SmartPlugModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Nancy;
+using SmartOutlet.Outlet;
+
+namespace SmartOutlet.Service.Modules
+{
+    public class SmartPlugModule : NancyModule
+    {
+        public SmartPlugModule(ISmartPlug plug) 
+            : base("plug")
+        {
+            Get("/", _ => GetState());
+            Post("/turn-on", _ => TurnPlugOn(plug));
+            Post("/turn-off", _ => TurnPlugOff(plug));
+        }
+
+        private static SmartPlugResponse GetState()
+        {
+            return new SmartPlugResponse
+            {
+                IsOn = Convert.ToBoolean(new Random().Next(0, 2)),
+                Name = "Plug 1"
+            };
+        }
+
+        private static PlugState TurnPlugOn(ISmartPlug plug)
+        {
+            ToggeResult result = plug.TurnOn();
+            return result.State;
+        }
+
+        private static PlugState TurnPlugOff(ISmartPlug plug)
+        {
+            ToggeResult result = plug.TurnOff();
+            return result.State;
+        }
+    }
+
+    public class SmartPlugResponse
+    {
+        public bool IsOn { get; set; }
+        public string Name { get; set; }
+    }
+}

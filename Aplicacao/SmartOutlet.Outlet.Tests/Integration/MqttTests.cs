5491b9010c5857ba92a7dada2feba3987444bbd5
Dyego Maas <dyego.maas@gmail.com>
2017-10-30 16:58:34 -0200

    Testes com M2MQTT, cliente C# para o protocolo MQTT.

---
+++ Aplicacao/SmartOutlet.Outlet.Tests/Integration/MqttTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using FluentAssertions;
+using NUnit.Framework;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace SmartOutlet.Outlet.Tests.Integration
+{
+    public class MqttTests
+    {
+        [Test]
+        public void um_teste()
+        {
+            var client = new MqttClient(
+                brokerHostName:"localhost",
+                brokerPort: 1883,
+                secure: false,
+                sslProtocol: MqttSslProtocols.None,
+                userCertificateSelectionCallback: null,
+                userCertificateValidationCallback: null
+            ); 
+//            var client = new MqttClient(IPAddress.Parse("10.0.0.9")); 
+            
+
+ 
+            var clientId = Guid.NewGuid().ToString();
+            var connected = client.Connect(clientId);
+            client.IsConnected.Should().BeTrue();
+            client.MqttMsgPublished += (sender, args) =>
+            {
+                Console.WriteLine($"Message published {args.MessageId}");
+            };
+
+            client.MqttMsgSubscribed += (sender, args) =>
+            {
+                Console.WriteLine($"Subscription: {args.MessageId}");
+            };
+
+            client.ConnectionClosed += (sender, args) =>
+            {
+                Console.WriteLine("Connection closed");
+            };
+            
+            
+            
+            client.MqttMsgPublishReceived += (sender, args) =>
+            {
+                Console.WriteLine($"message received: {Encoding.UTF8.GetString(args.Message)}, duplicated? {args.DupFlag}");
+            }; 
+            var subscriptionId = client.Subscribe(new string[] { "/home/temperature" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            Console.WriteLine($"Subscription Id: {subscriptionId}");
+ 
+// publish a message on "/home/temperature" topic with QoS 2 
+            var messageId = client.Publish("/home/temperature", Encoding.UTF8.GetBytes("25"), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true);
+            Console.WriteLine($"Publish MessageId {messageId}");
+
+            Thread.Sleep(3000);
+            
+            client.Disconnect();
+
+//            Console.Read();
+
+// register to message received 
+
+
+// subscribe to the topic "/home/temperature" with QoS 2 
+        }
+        
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
+        {
+// handle message received
+        } 
+    }
+}
--- Aplicacao/SmartOutlet.Service/NancyService.cs
+++ Aplicacao/SmartOutlet.Service/NancyService.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Text;
 using Marten;
 using Nancy.Hosting.Self;
 using Quartz;
 using Quartz.Impl;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace SmartOutlet.Service
 {
@@ -11,17 +14,63 @@ namespace SmartOutlet.Service
         private const string ServiceUri = "http://localhost:8001/smart-things/";
         private readonly NancyHost _nancyHost;
         private readonly IScheduler _scheduler;
+        private readonly MqttClient _mqttClient;
 
         public NancyService()
         {
             _nancyHost = new NancyHost(new Uri(ServiceUri));
             _scheduler = new StdSchedulerFactory().GetScheduler();
+            _mqttClient = new MqttClient(
+                brokerHostName:"localhost",
+                brokerPort: 1883,
+                secure: false,
+                sslProtocol: MqttSslProtocols.None,
+                userCertificateSelectionCallback: null,
+                userCertificateValidationCallback: null
+            ); 
         }
 
         public void Start()
         {
             ConfigureNancy();
             ConfigureJob();
+            ConfigureMqtt();
+        }
+        
+        public void ConfigureMqtt()
+        {
+            var clientId = Guid.NewGuid().ToString();
+            var connected = _mqttClient.Connect(clientId);
+            
+            _mqttClient.MqttMsgPublished += (sender, args) =>
+            {
+                Console.WriteLine($"Message published {args.MessageId}");
+            };
+
+            _mqttClient.MqttMsgSubscribed += (sender, args) =>
+            {
+                Console.WriteLine($"Subscription: {args.MessageId}");
+            };
+
+            _mqttClient.ConnectionClosed += (sender, args) =>
+            {
+                Console.WriteLine("Connection closed");
+            };
+
+            _mqttClient.MqttMsgUnsubscribed += (sender, args) =>
+            {
+                Console.WriteLine($"Unsubscription: {args.MessageId}");
+            };
+
+            _mqttClient.MqttMsgPublishReceived += (sender, args) =>
+            {
+                Console.WriteLine($"message received: {Encoding.UTF8.GetString(args.Message)} in topic {args.Topic}");
+            }; 
+            
+            var subscriptionId = _mqttClient.Subscribe(new[] { "/home/temperature" }, new[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+ 
+// publish a message on "/home/temperature" topic with QoS 2 
+            _mqttClient.Publish("/home/temperature", Encoding.UTF8.GetBytes("25"), MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE, true); 
         }
 
         private void ConfigureNancy()
@@ -45,6 +94,7 @@ namespace SmartOutlet.Service
         {
             _nancyHost.Dispose();
             _scheduler.Shutdown();
+            _mqttClient.Disconnect();
         }
     }
 }

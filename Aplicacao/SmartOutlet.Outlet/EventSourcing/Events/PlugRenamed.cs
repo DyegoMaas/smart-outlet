61e4e5fea8741e292d9499e231fd2aba7b0bad7e
Dyego Maas <dyego.maas@gmail.com>
2017-11-02 10:40:27 -0200

    Criado evento para renomear o dispositivo.

---
+++ Aplicacao/SmartOutlet.Outlet/EventSourcing/Events/PlugRenamed.cs
@@ -0,0 +1,16 @@
+﻿namespace SmartOutlet.Outlet.EventSourcing.Events
+{
+    public class PlugRenamed
+    {
+        public string NewName { get; set; }
+
+        public PlugRenamed(string newName)
+        {
+            NewName = newName;
+        }
+
+        protected PlugRenamed()
+        {
+        }
+    }
+}

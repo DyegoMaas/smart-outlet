72810f14a0287af25a7097185df2a4964ca484f1
Dyego Maas <dyego.maas@gmail.com>
2017-11-22 11:14:26 -0200

    Movendo arquivos.

--- Aplicacao/SmartOutlet.Outlet/EventSourcing/AggregationRoots/Timeline.cs
+++ Aplicacao/SmartOutlet.Outlet/EventSourcing/AggregationRoots/Timeline.cs
@@ -42,34 +42,4 @@ namespace SmartOutlet.Outlet.EventSourcing.AggregationRoots
             return _eventDescriptions;
         }
     }
-
-    public static class PlugEventExtensions
-    {
-        public static string GetDescription(this IPlugEvent @event)
-        {
-            return $"{@event.IssuedAt:yyyy/MM/dd HH:mm:ss} - {DescribeEvent(@event)}";
-        }
-
-        private static string DescribeEvent(IPlugEvent @event)
-        {
-            switch (@event)
-            {
-                case PlugActivated _:
-                    return "Plugue ativado";
-                case PlugRenamed _:
-                    return "Plugue renomeado para " + ((PlugRenamed) @event).NewName;
-                case PlugTurnedOn _:
-                    return "Plugue ligado";
-                case PlugTurnedOff _:
-                    return "Plugue desligado";
-                case OperationScheduled agendamento:
-                    return agendamento.Type == CommandType.TurnOn
-                        ? $"Ligar em {(int) agendamento.TimeInFuture.TotalSeconds}s ({agendamento.IssuedAt + agendamento.TimeInFuture:yyyy/MM/dd HH:mm:ss})"
-                        : $"Desligar em {(int) agendamento.TimeInFuture.TotalSeconds}s ({agendamento.IssuedAt + agendamento.TimeInFuture:yyyy/MM/dd HH:mm:ss})";
-            }
-
-            return string.Empty;
-        }
-    }
-
 }
---
+++ Aplicacao/SmartOutlet.Outlet/EventSourcing/Events/PlugEventExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace SmartOutlet.Outlet.EventSourcing.Events
+{
+    public static class PlugEventExtensions
+    {
+        public static string GetDescription(this IPlugEvent @event)
+        {
+            return $"{@event.IssuedAt:yyyy/MM/dd HH:mm:ss} - {DescribeEvent(@event)}";
+        }
+
+        private static string DescribeEvent(IPlugEvent @event)
+        {
+            switch (@event)
+            {
+                case PlugActivated _:
+                    return "Plugue ativado";
+                case PlugRenamed _:
+                    return "Plugue renomeado para " + ((PlugRenamed) @event).NewName;
+                case PlugTurnedOn _:
+                    return "Plugue ligado";
+                case PlugTurnedOff _:
+                    return "Plugue desligado";
+                case OperationScheduled agendamento:
+                    return agendamento.Type == CommandType.TurnOn
+                        ? $"Ligar em {(int) agendamento.TimeInFuture.TotalSeconds}s ({agendamento.IssuedAt + agendamento.TimeInFuture:yyyy/MM/dd HH:mm:ss})"
+                        : $"Desligar em {(int) agendamento.TimeInFuture.TotalSeconds}s ({agendamento.IssuedAt + agendamento.TimeInFuture:yyyy/MM/dd HH:mm:ss})";
+            }
+
+            return string.Empty;
+        }
+    }
+}
